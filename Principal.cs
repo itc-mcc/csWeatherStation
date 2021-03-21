1ca4e1e5bba679582ba4ae5f81ff94ad800653f3
Ricardo Quintero <30350282+ricardo-quintero@users.noreply.github.com>
2021-03-20 19:36:01 -0700

    Init App ...

---
+++ Principal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HeadFirstDesignPatterns.Observer.WeatherData;
+
+namespace WeatherStation
+{
+    class Principal
+    {
+        public static void Main()
+        {
+            WeatherData weatherData = new WeatherData();
+
+            CurrentConditionsDisplay currentConditionsDisplay   = new CurrentConditionsDisplay(weatherData);
+            StatisticsDisplay        statisticsDisplay          = new StatisticsDisplay(weatherData);
+            ForcastDisplay           forcastDisplay             = new ForcastDisplay(weatherData);
+            HeatIndexDisplay         heatIndexDisplay           = new HeatIndexDisplay(weatherData);
+
+            Console.WriteLine("The Weather Station App...");
+
+            weatherData.SetMeasurements(80, 65, 30.4f);
+            weatherData.SetMeasurements(82, 70, 29.2f);
+            weatherData.SetMeasurements(78, 90, 29.2f);
+
+            Console.ReadLine();
+        }
+    }
+}
